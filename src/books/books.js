1df5e21476c334197e921e38eab3d6d0950a6601
Saron <saronnhong@gmail.com>
2021-03-11 00:03:36 -0800

    add search and exclude read from text files

---
+++ src/books/books.js
@@ -0,0 +1,34 @@
+import raw from "raw.macro";
+
+export const BOOKS = [
+    {
+        id: 1,
+        title: "Warrior Maid of Mars",
+        author: "Alfred Coppel",
+        location: raw('./Warrior_Maid_Of_Mars.txt'),
+    },
+    {
+        id: 2,
+        title: "The Rocketeers Have Shaggy Ears",
+        author: "Keith Bennett",
+        location: raw('./The_Rocketeers_Have_Shaggy_Ears.txt'),
+    },
+    {
+        id: 3,
+        title: "He that will not when he may; vol. I",
+        author: "Mrs. Margaret Oliphant",
+        location: raw('./He_That_Will_Not_When_He_May.txt'),
+    },
+    {
+        id: 4,
+        title: "Some Historic Trees",
+        author: "Anonymous",
+        location: raw('./Some_Historic_Trees.txt'),
+    },
+    {
+        id: 5,
+        title: "The Last Two Alive",
+        author: "Alfred Coppel",
+        location: raw('./The_Last_Two_Alive.txt'),
+    },
+];
---
+++ src/components/Reports/reports.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import "./style.css";
+class Reports extends Component {
+
+    render() {
+        return (
+            <div>
+                reports
+            </div>
+        )
+    }
+}
+
+export default Reports;

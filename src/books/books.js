34b1c540afbce7fc6f2c93fb34cadade535aa9b4
Saron <saronnhong@gmail.com>
2021-03-11 21:16:17 -0800

    add tags to table, display first 200characters in table, add 20books

--- src/books/books.js
+++ src/books/books.js
@@ -36,4 +36,116 @@ export const BOOKS = [
         location: raw('./The_Last_Two_Alive.txt'),
         url: 'http://gutenberg.org/files/64759/64759-0.txt'
     },
+    {
+        id: 5,
+        title: "Airman: His Life And Work",
+        author: "Muriel Hawker",
+        location: raw('./Airman.txt'),
+        url: 'http://gutenberg.org/files/64793/64793-0.txt'
+    },
+    {
+        id: 6,
+        title: "Ticket To Paradise",
+        author: "D. L. James",
+        location: raw('./Ticket_To_Paradise.txt'),
+        url: 'http://gutenberg.org/files/64791/64791-0.txt'
+    },
+    {
+        id: 7,
+        title: "Lord Of The Silent Death",
+        author: "Robert Moore Williams",
+        location: raw('./Lord_Of_The_Silent_Death.txt'),
+        url: 'http://gutenberg.org/files/64777/64777-0.txt'
+    },
+    {
+        id: 8,
+        title: "Bratton's Idea",
+        author: "Manly Wade Wellman",
+        location: raw('./Brattons_Idea.txt'),
+        url: 'http://gutenberg.org/files/64789/64789-0.txt'
+    },
+    {
+        id: 9,
+        title: "The Secret Dispatch",
+        author: "James Grant",
+        location: raw('./The_Secret_Dispatch.txt'),
+        url: 'http://gutenberg.org/files/64788/64788-0.txt'
+    },
+    {
+        id: 10,
+        title: "The Spanish Influenza Pandemic of 1918",
+        author: "Oscar Jewell Harvey",
+        location: raw('./The_Spanish_Influenze_Pandemic.txt'),
+        url: 'http://gutenberg.org/files/64785/64785-0.txt'
+    },
+    {
+        id: 11,
+        title: "Mortal Summer",
+        author: "Mark Van Doren",
+        location: raw('./Mortal_Summer.txt'),
+        url: 'http://gutenberg.org/files/64783/64783-0.txt'
+    },
+    {
+        id: 12,
+        title: "A Modest Proposal",
+        author: "Jonathan Swift",
+        location: raw('./Modest_Proposal.txt'),
+        url: 'http://gutenberg.org/files/1080/1080-0.txt'
+    },
+    {
+        id: 13,
+        title: "The Adventures of Sherlock Holmes",
+        author: "Arthur Conan Doyle",
+        location: raw('./Sherlock_Holmes.txt'),
+        url: 'http://gutenberg.org/files/1661/1661-0.txt'
+    },
+    {
+        id: 14,
+        title: "Beowulf An Anglo-Saxon Epic Poem",
+        author: "Lesslie Hall",
+        location: raw('./Beowulf.txt'),
+        url: 'http://gutenberg.org/files/16328/16328-0.txt'
+    },
+    {
+        id: 15,
+        title: "The Odyssey",
+        author: "Homer",
+        location: raw('./Odyssey.txt'),
+        url: 'http://gutenberg.org/files/1727/1727-0.txt'
+    },
+    {
+        id: 16,
+        title: "The Jungle Book",
+        author: "Rudyard Kipling",
+        location: raw('./Jungle_Book.txt'),
+        url: 'http://gutenberg.org/files/236/236-0.txt'
+    },
+    {
+        id: 17,
+        title: "The Complete Works of William Shakespeare",
+        author: "William Shakespeare",
+        location: raw('./William_Shakespeare.txt'),
+        url: 'http://gutenberg.org/files/100/100-0.txt'
+    },
+    {
+        id: 18,
+        title: "The Nursery Rhymes of England",
+        author: "Various",
+        location: raw('./Nursery_Rhymes.txt'),
+        url: 'http://gutenberg.org/cache/epub/32415/pg32415.txt'
+    },
+    {
+        id: 19,
+        title: "The Memoirs of Sherlock Holmes",
+        author: "Arthur Conan Doyle",
+        location: raw('./Memoirs_Of_Sherlock.txt'),
+        url: 'http://gutenberg.org/files/834/834-0.txt'
+    },
+    {
+        id: 20,
+        title: "Grimm's Fairy Tales",
+        author: "Various",
+        location: raw('./Grimms_Fairy_Tales.txt'),
+        url: 'http://gutenberg.org/files/52521/52521-0.txt'
+    },
 ];
--- src/components/Reports/reports.js
+++ src/components/Reports/reports.js
@@ -16,7 +16,7 @@ class Reports extends Component {
                         <thead class="table-dark">
                             <tr>
                                 <th scope="col">ID</th>
-                                <th scope="col">Books</th>
+                                <th scope="col">Medical Reports</th>
                                 <th scope="col">Tags</th>
                             </tr>
                         </thead>
@@ -24,8 +24,10 @@ class Reports extends Component {
                             {this.props.searchResults.map((book) =>
                                 <tr onClick={() => this.props.onClick(book)}>
                                     <th scope="row">{book.id}</th>
-                                    <td>{book.title} by {book.author}</td>
-                                    <td></td>
+                                    <td>{BOOKS[book.id].location.substring(0,200)}...</td>
+                                    {/* <td>{book.title} by {book.author}</td> */}
+                                    <td>{this.props.tags.goodreport.includes(book.id) ? "#goodreport" : null}
+                                    {this.props.tags.conditionpresent.includes(book.id) ? " #conditionpresent" : null}</td>
                                 </tr>
                             )}
                         </tbody>
@@ -42,15 +44,9 @@ class Reports extends Component {
                         {this.props.tags.conditionpresent.includes(this.props.idToPass) ?
                             <button type="button" class="btn btn-danger tagbutton" onClick={() => this.props.removeTag("conditionpresent", this.props.idToPass)}>- conditionpresent</button>
                             : <button type="button" class="btn btn-success tagbutton" onClick={() => this.props.addTag("conditionpresent", this.props.idToPass)}>+ conditionpresent</button>}
-                        {/* <button onClick={()=>this.props.addTag("conditionpresent", this.props.idToPass)}>
-                        {this.props.tags.conditionpresent[this.props.idToPass] ?
-                            "+ conditionpresent"
-                            : "- conditionpresent"}
-                        </button> */}
                         <iframe title="some value" width="100%" height="800px" src={`${BOOKS[this.props.idToPass].url}`}></iframe>
                     </div>
                     : null}
-
             </div>
         )
     }
